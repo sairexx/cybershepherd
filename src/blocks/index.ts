b6bb0771858356d078cafcd1edce4e3bcc9e91b7
Sairex <biggdadddy1999@gmail.com>
2023-02-08 02:17:51 +0700

    Add header(tablet, desktop). Add footer(not complete). Add main-preview(mobile, not completed)

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,3 +1,11 @@
 import './header'
 import './content'
 import './footer'
+
+import './main/1preview'
+import './main/2info'
+import './main/3generator'
+import './main/4catalog'
+import './main/5reasons'
+import './main/6partners'
+import './main/7reviews'
